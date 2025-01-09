544694b22458ab565cfb3ecec60b26b4f8501abd
Deepanshu Mehra <deepanshumehra2102@gmail.com>
2025-01-09 12:32:47 +0530

    feat: added ts implementation of all test cases

--- typescript-implementation/src/calculator.ts
+++ typescript-implementation/src/calculator.ts
@@ -2,8 +2,23 @@ export class Calculator {
   add(input: string): number {
     if (!input) return 0;
 
-    const numArray = input.split(",").map(Number);
-    return numArray.reduce((acc, currVal) => acc + currVal, 0);
+    let delimiters = /[\n,]/;
+    let newString = input;
+    if (input.startsWith("//")) {
+      const parts = newString.split("\n");
+      delimiters = new RegExp(`[${parts[0].slice(2)}]`);
+      newString = parts[1];
+    }
+
+    const numArray = input.split(delimiters).map(Number);
+    const negativeNum = numArray.filter((num) => num < 0);
+    if (negativeNum.length > 0) {
+      throw new Error(`negatives not allowed: ${negativeNum.join(", ")}`);
+    }
+
+    return numArray
+      .filter((num) => num <= 1000)
+      .reduce((acc, currVal) => acc + currVal, 0);
   }
 }
 
--- typescript-implementation/src/test/calculator.test.ts
+++ typescript-implementation/src/test/calculator.test.ts
@@ -13,5 +13,36 @@ describe("A String calculator in typescript", () => {
     test("should return the sum of two numbers separated by a comma", () => {
       expect(calculator.add("1,5")).toBe(6);
     });
+
+    test("should return the sum of all the numbers present inside string seprated by comma", () => {
+      expect(calculator.add("1,3,4,5,2,6,8,9,1")).toBe(39);
+    });
+
+    test("should handle new lines between numbers", () => {
+      expect(calculator.add("1\n2,3")).toBe(6);
+      expect(calculator.add("1\n2\n3")).toBe(6);
+    });
+
+    test("should support different delimiters", () => {
+      expect(calculator.add("//;\n1;2")).toBe(3);
+      expect(calculator.add("//;\n1;2;3;4;6")).toBe(16);
+      expect(calculator.add("//?\n1?2?3?4?6")).toBe(16);
+    });
+
+    test("should ignore numbers bigger than 1000", () => {
+      expect(calculator.add("2,1001")).toBe(2);
+      expect(calculator.add("//;\n1;2;1000;4;1006")).toBe(1007);
+    });
+  });
+
+  describe("an invalid string input", () => {
+    test("should throw an exception for negative numbers", () => {
+      expect(() => calculator.add("1,-2,3")).toThrow(
+        "negatives not allowed: -2"
+      );
+      expect(() => calculator.add("//;\n1;-2;3;-6;-5")).toThrow(
+        "negatives not allowed: -2, -6, -5"
+      );
+    });
   });
 });
