9a8c4d9c334545e7dfd9a4ebb1343370279211f5
Deepanshu Mehra <deepanshumehra2102@gmail.com>
2025-01-09 11:05:52 +0530

    feat: added implementation for handling 1 or 2 number input string

--- javascript-implementation/calculator.js
+++ javascript-implementation/calculator.js
@@ -1,6 +1,9 @@
 class StringCalculator {
   add(string) {
-    return 0;
+    if (!string) return 0;
+
+    const numArr = string.split(",").map(Number);
+    return numArr.reduce((acc, curValue) => acc + curValue, 0);
   }
 }
 
