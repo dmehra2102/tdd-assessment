1493aaf90088adf454aa6f932d48bb984380367d
Deepanshu Mehra <deepanshumehra2102@gmail.com>
2025-01-09 12:25:30 +0530

    test-ts : added test for input containing 1 or 2 numbers

--- typescript-implementation/src/calculator.ts
+++ typescript-implementation/src/calculator.ts
@@ -1,6 +1,9 @@
 export class Calculator {
-  add(input: string): number | undefined {
+  add(input: string): number {
     if (!input) return 0;
+
+    const numArray = input.split(",").map(Number);
+    return numArray.reduce((acc, currVal) => acc + currVal, 0);
   }
 }
 
--- typescript-implementation/src/test/calculator.test.ts
+++ typescript-implementation/src/test/calculator.test.ts
@@ -5,5 +5,13 @@ describe("A String calculator in typescript", () => {
     test("should return 0 for an empty string", () => {
       expect(calculator.add("")).toBe(0);
     });
+
+    test("should return the number itself if there is only one number", () => {
+      expect(calculator.add("1")).toBe(1);
+    });
+
+    test("should return the sum of two numbers separated by a comma", () => {
+      expect(calculator.add("1,5")).toBe(6);
+    });
   });
 });
