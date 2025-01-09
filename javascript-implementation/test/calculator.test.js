547676a6fe36c9916bac9069c1c4f81daae2c746
Deepanshu Mehra <deepanshumehra2102@gmail.com>
2025-01-09 10:30:15 +0530

    test : added first test case for empty string input

---
+++ javascript-implementation/test/calculator.test.js
@@ -0,0 +1,9 @@
+describe("A String calculator", () => {
+  describe("a valid string input", () => {});
+
+  describe("an invalid string input", () => {
+    test("should return 0 for an empty string", () => {
+      expect(calculator.add("")).toBe(0);
+    });
+  });
+});
